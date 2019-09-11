46e50f8e898681095aefc3b703b708ab0a56c9d8
munnafdewan <engr.munnaf@gmail.com>
2019-09-11 15:14:32 +0600

    testfile

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What Is your Name?");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("How old are you ?");
+            string Age = Console.ReadLine();
+
+            Console.WriteLine("Enter your phone number:");
+            string Contact = Console.ReadLine();
+
+            Console.WriteLine("Enter your Address:");
+            string address = Console.ReadLine();
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("My name is " + Name + Environment.NewLine + "I am " + Age + Environment.NewLine + "My phone number is " + Contact + Environment.NewLine + "I am from " + address + Environment.NewLine + "Thank You for Giving me Your information");
+            Console.ReadKey();
+        }
+    }
+}
